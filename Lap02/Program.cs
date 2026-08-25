68d9dbc22a117fe2804660b8ddb2dfbe78e90936
ratchanonchaloklang <ratchanon.chal@bumail.net>
2026-08-25 10:45:18 +0700

    Add lap02

---
+++ Lap02/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * Student ID :1690703168
+ * Name       :Ratchanon Chaloklang
+ * Section    :129c
+ * No.        :N/A
+ * Course     : GI113 Computer Programming (GI)
+ */
+namespace Lap02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
